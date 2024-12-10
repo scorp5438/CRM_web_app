0bcc8b356ebb7edcadcb42a7a183680419c87e31
ScOrP <alex_77_90@mail.ru>
2024-12-10 17:05:04 +0500

    create model, serializer and view  Exam

---
+++ CRM_app/testing/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError as RestFrameworkValidationError
+from django.contrib.auth.models import User
+
+from .models import Exam
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Exam с подстановкой данных в поле name_examiner
+    по фильтру company=1 (ДМ), post='OKK'.
+    """
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+    def validate(self, data):
+        try:
+            return super().validate(data)
+        except ValidationError as e:
+            raise RestFrameworkValidationError(e.detail)
