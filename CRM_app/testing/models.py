e207e9c40e881b4fb29bc184da8e803a18d61e2b
ScOrP <alex_77_90@mail.ru>
2024-12-13 17:28:52 +0500

    Double quotes are replaced with single quotes

--- CRM_app/testing/models.py
+++ CRM_app/testing/models.py
@@ -12,12 +12,12 @@ FULL_NAME_PATTERN = r'^[А-ЯЁ][а-яё]+(-[А-ЯЁ][а-яё]+)?\s[А-ЯЁ][а-
 
 def validate_name_intern(value):
     if not re.search(FULL_NAME_PATTERN, value):
-        raise ValidationError("Введены некорректные данные, введите значение в формате: Фамилия Имя")
+        raise ValidationError('Введены некорректные данные, введите значение в формате: Фамилия Имя')
 
 
 def validate_date_exam(value):
     if value < datetime.today().date():
-        raise ValidationError("Дата зачета не может быть в прошлом")
+        raise ValidationError('Дата зачета не может быть в прошлом')
 
 
 class Exam(models.Model):
@@ -34,33 +34,33 @@ class Exam(models.Model):
         ('Универсал', 'Универсал'),
     ]
 
-    date_exam = models.DateField(blank=False, verbose_name="Дата зачета", validators=[validate_date_exam])
-    name_intern = models.CharField(max_length=60, blank=False, verbose_name="Фамилия Имя стажера",
+    date_exam = models.DateField(blank=False, verbose_name='Дата зачета', validators=[validate_date_exam])
+    name_intern = models.CharField(max_length=60, blank=False, verbose_name='Фамилия Имя стажера',
                                    validators=[validate_name_intern])
     company = models.ForeignKey(Companies, on_delete=models.PROTECT, verbose_name='Компания', null=False,
                                 related_name='exams')
     training_form = models.CharField(max_length=60, blank=False, choices=training_forms_list,
-                                     verbose_name="Форма обучения")
+                                     verbose_name='Форма обучения')
     try_count = models.PositiveSmallIntegerField(blank=False, choices=[(i, i) for i in range(1, 4)],
-                                                 verbose_name="Попытка")
-    time_exam = models.TimeField(blank=True, default="00:00",
-                                 verbose_name="Время зачета")  # TODO Продумать что с ним сделать
+                                                 verbose_name='Попытка')
+    time_exam = models.TimeField(blank=True, default='00:00',
+                                 verbose_name='Время зачета')  # TODO Продумать что с ним сделать
     name_examiner = models.ForeignKey(User, blank=True, null=True, on_delete=models.PROTECT,
-                                      verbose_name="ФИ сотрудника", related_name='name_examiner')
+                                      verbose_name='ФИ сотрудника', related_name='name_examiner')
     result_exam = models.CharField(max_length=25, blank=True, choices=result_list, default='',
-                                   verbose_name="Результат")
-    comment_exam = models.TextField(max_length=2000, blank=True, verbose_name="комментарий")
+                                   verbose_name='Результат')
+    comment_exam = models.TextField(max_length=2000, blank=True, verbose_name='комментарий')
     name_train = models.ForeignKey(User, blank=False, null=True, on_delete=models.PROTECT,
-                                   verbose_name="ФИ обучающего/обучающих", related_name='name_train')
+                                   verbose_name='ФИ обучающего/обучающих', related_name='name_train')
     internal_test_examiner = models.ForeignKey(User, blank=False, null=True, on_delete=models.PROTECT,
-                                               verbose_name="ФИ принимающего внутреннее ТЗ",
+                                               verbose_name='ФИ принимающего внутреннее ТЗ',
                                                related_name='internal_test_examiner')
-    note = models.CharField(max_length=255, blank=True, verbose_name="Примечание")
+    note = models.CharField(max_length=255, blank=True, verbose_name='Примечание')
 
     class Meta:
         verbose_name = 'Зачет'
         verbose_name_plural = 'Зачеты'
-        unique_together = ["date_exam", "time_exam", "name_examiner"]
+        unique_together = ['date_exam', 'time_exam', 'name_examiner']
 
     def __str__(self):
         return f'{self.name_intern} {self.company}'
