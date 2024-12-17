2f9e84172394519f963ed91806a01bc94766d75d
vavil <vavilonskiy99@mail.ru>
2024-12-17 14:39:06 +0500

    text

--- CRM_app/testing/views.py
+++ CRM_app/testing/views.py
@@ -10,7 +10,6 @@ from .serializers import ExamSerializer
 from .models import Exam
 from profiles.models import Companies
 
-
 # prefetch_related('exams', 'user')
 
 class ExamView(viewsets.ModelViewSet):
@@ -24,16 +23,16 @@ class ExamView(viewsets.ModelViewSet):
         last_day_of_month = (first_day_of_month + timedelta(days=32)).replace(day=1) - timedelta(days=1)
 
         if self.request.user.is_staff:
-            if self.request.get('mode'):
-                queryset = Exam.objects.filter(name_examiner=self.request.user.id, result_exam='',
-                                               date_exam=now).select_related('company', 'name_train',
-                                                                             'internal_test_examiner')
-            else:
-                company = Companies.objects.filter(slug='kompaniya-1').first()
-                # TODO Сделать и проверить фильтрацию екзаменов по КЦ для МэйнКомпании
-                queryset = Exam.objects.filter(company=company.id, date_exam__gte=first_day_of_month,
-                                               date_exam__lte=last_day_of_month).select_related('company', 'name_train',
-                                                                                                'internal_test_examiner')
+            # if self.request.get('mode'):
+            #     queryset = Exam.objects.filter(name_examiner=self.request.user.id, result_exam='',
+            #                                    date_exam=now).select_related('company', 'name_train',
+            #                                                                  'internal_test_examiner')
+            # else:
+            company = Companies.objects.filter(slug='kompaniya-1').first()
+            # TODO Сделать и проверить фильтрацию екзаменов по КЦ для МэйнКомпании
+            queryset = Exam.objects.filter(company=company.id, date_exam__gte=first_day_of_month,
+                                           date_exam__lte=last_day_of_month).select_related('company', 'name_train',
+                                                                                            'internal_test_examiner')
         else:
             company = self.request.user.profiles.company
             queryset = Exam.objects.filter(company=company.id, date_exam__gte=first_day_of_month,
