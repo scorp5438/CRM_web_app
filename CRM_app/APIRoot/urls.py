1f23409b74c5afc597362fefdf02f9d6eaae564e
Punisher <alex_77_90@mail.ru>
2025-01-24 20:11:18 +0500

    add serializer, view and url for api mistakes

--- CRM_app/APIRoot/urls.py
+++ CRM_app/APIRoot/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from profiles.api.views import CompaniesApiView, UserExamApiView, AdminCcApiView, AdminMainApiView
 from testing.api.views import ExamApiView, ExamCreateApiView, ExamUpdateApiView, ResultApiView
-from checklists.api.views import MistakApiView
+from checklists.api.views import MistakeApiView
 from .view import TestCSRFView
 
 router = DefaultRouter()
@@ -16,7 +16,7 @@ router.register(r'update_exam', ExamUpdateApiView, basename='update_exam')
 router.register(r'admin_cc', AdminCcApiView, basename='admin_cc')
 router.register(r'admin_main', AdminMainApiView, basename='admin_main')
 router.register(r'results', ResultApiView, basename='results')
-router.register(r'mistakes', MistakApiView, basename='mistakes')
+router.register(r'mistakes', MistakeApiView, basename='mistakes')
 
 urlpatterns = [
     path('get-csrf-token/', TestCSRFView.get_csrf_token, name='get-csrf-token'),
