131f670f8809c33746dd35beeef74bf225765eda
Julia-Troyan <147001746+Julia-Troyan@users.noreply.github.com>
2025-04-20 17:52:17 +0300

    Починили Logout

--- CRM_app/profiles/urls.py
+++ CRM_app/profiles/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
 
-from .views import MyLoginView, index, logout_view
+from .views import MyLoginView, index, MyLogoutView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('login/',
          MyLoginView.as_view(
-
+             template_name='index.html',
+             redirect_authenticated_user=True
          ), name='login'),
-#     path('logout/', MyLogoutView.as_view(), name='logout'),
-    path('logout', logout_view, name='logout'),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
     path('', index, name='index')
 ]
--- CRM_app/profiles/views.py
+++ CRM_app/profiles/views.py
@@ -1,18 +1,23 @@
 import json
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
-from django.http import JsonResponse
+
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views import View
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 @login_required
 def index(request):
      return render(request, 'index.html', {})
 
-class MyLoginView(View):
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class MyLoginView(LoginView):
+
     def post(self, request, *args, **kwargs):
         try:
             # Получаем данные из JSON
@@ -40,19 +45,5 @@ class MyLoginView(View):
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'error': 'Неверный формат JSON'}, status=400)
 
-# class MyLogoutView(View):
-#     next_page = reverse_lazy('myauth_profiles:login')
-# views.py
-from django.contrib.auth import logout
-from django.http import JsonResponse
-
-
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return JsonResponse({'message': 'Logged out'})
-    return JsonResponse({'error': 'Method not allowed'}, status=405)
-
-
-
-
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy('myauth_profiles:login')
